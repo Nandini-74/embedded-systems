1ccf4dfddca0fa9fc753f992b60b2f87695e65fb
Nandini-74 <jillellamudinandini@gmail.com>
2026-07-02 23:34:09 +0530

    Add first C program

---
+++ C/Basics/student_profile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+printf("========= STUDENT PROFILE ===========\n");
+printf("Name : Nandu\n");
+printf("Branch : ECE\n");
+printf("Start : July 2026\n");
+printf("College : VMTW\n");
+
+return 0;
+  }
